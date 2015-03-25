35f0d49fb3c1375fdba643cbd2fe7898d7467c71
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-03-25 22:07:32 +0300

    Support +, -, *, /

--- src/careepcup/fb/Main.java
+++ src/careepcup/fb/Main.java
@@ -4,20 +4,52 @@ public class Main {
 
     // http://www.careercup.com/question?id=4911380140392448
     public static void main(String[] args) {
-        // An equation with +, -, /, *
-        // "1*5*4+8*9+16/8-9"; An old equation
-        String equation = "2+7-21-3*2/1"; // 83
-        double result = computeEquation(equation);
+        String equation = "1*5*4+8*9+16";
+        int result = compute(equation);
         System.out.println(result);
+
+        String equationWithAllSigns = "-1*5*4/2-8*9-16+1";
+        double allSignsResult = computeWithAllOperations(equationWithAllSigns);
+        System.out.println(allSignsResult);
+
     }
 
-    private static double computeEquation(String input) {
-        String[] parts = input.split("(?=[/*])|(?<=[/*])");
-        double result = computeSum(parts[0]);
+    private static int compute(String equation) {
+        int result = 0;
+        String[] byPluses = equation.split("\\+");
+        for (String multipl : byPluses) {
+            String[] byMultipl = multipl.split("\\*");
+            int multiplResult = 1;
+            for (String operand : byMultipl) {
+                multiplResult *= Integer.parseInt(operand);
+            }
+            result += multiplResult;
+        }
+        return result;
+    }
 
+    private static double computeWithAllOperations(String input) {
+        double result = 0.0;
+        /*
+         * 1. Split the input string by +
+         * 2. Calculate the products and quotients
+         * 3. Sum up the results
+         */
+
+        // (\\d)- is used instead of - to support strings starting with -
+        String parts[] = input.replaceAll("(\\d)-", "$1+-").split("\\+");
+        for (String part : parts) {
+            result += computeMultAndDivision(part);
+        }
+        return result;
+    }
+
+    private static double computeMultAndDivision(String part) {
+        String[] parts = part.split("(?=[/*])|(?<=[/*])");
+        double result = Double.parseDouble(parts[0]);
         for (int i = 1; i < parts.length; i += 2) {
             String op = parts[i];
-            double val = computeSum(parts[i + 1]);
+            double val = Double.parseDouble(parts[i + 1]);
             switch (op) {
             case "*":
                 result *= val;
@@ -26,17 +58,6 @@ public class Main {
                 result /= val;
                 break;
             }
-        }
-        return result;
-    }
-    
-    private static double computeSum(String input) {
-        // TODO: write a comment
-        input = input.replace("-", "+-");
-        String []parts = input.split("\\+");
-        double result = 0.0;
-        for (String operand : parts) {
-            result += Double.parseDouble(operand);
         }
         return result;
     }
